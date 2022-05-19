a9f0f87c5c5b6760ba71e0665d409ea34e8e4c09
Ruben <201902051@estudantes.ips.pt>
2022-05-19 17:06:53 +0200

    update

--- KnifesAway/Assets/CameraSwitch.cs
+++ KnifesAway/Assets/CameraSwitch.cs
@@ -1,18 +1,23 @@
 using Cinemachine;
 using System;
 using UnityEngine;
+using System.Linq;
 
 public class CameraSwitch : MonoBehaviour
 {
     public GameObject thrower;
+    public GameObject deadGuy;
     public CinemachineVirtualCamera[] cameras;
     private int currentIdx;
+    private bool lastCamera;
 
     // Start is called before the first frame update
     void Start()
     {
         currentIdx = 0;
+        lastCamera = false;
         foreach (var item in cameras) item.gameObject.SetActive(true);
+        CinemachineCore.GetInputAxis = GetAxisCustom;
     }
 
     // Update is called once per frame
@@ -26,7 +31,7 @@ public class CameraSwitch : MonoBehaviour
     {
         if (currentIdx == cameras.Length) 
             return;
-
+        
         for(int i=0; i<cameras.Length; i++)
         {
             if (i <= currentIdx)
@@ -36,9 +41,26 @@ public class CameraSwitch : MonoBehaviour
             {
                 currentIdx++;
                 cameras[i].Priority = 1;
+                if (i == cameras.Length - 1) RotateAroundPlayer();
                 break;
             }
         }
-        
+    }
+
+    public float GetAxisCustom(string axisName)
+    {
+        return 0;
+    }
+
+    private void LateUpdate()
+    {
+        if (!lastCamera) return;
+        var camera = cameras.Last();
+        camera.transform.RotateAround(deadGuy.transform.position, new Vector3(0, 1, 0), 2 * Time.deltaTime);
+    }
+
+    private void RotateAroundPlayer()
+    {
+        lastCamera = true;
     }
 }
--- KnifesAway/Assets/onHit.cs
+++ KnifesAway/Assets/onHit.cs
@@ -7,16 +7,34 @@ public class onHit : MonoBehaviour{
     public GameObject parent;
     public Animator deadGuyAnimator;
     public GameObject placeholder;
+    public GameObject mainCamera;
+    public GameObject particle;
+    public AudioSource music1;
+    public AudioSource music2;
+
+    void Start()
+    {
+        music1.playOnAwake = true;
+    }
 
     private void OnCollisionEnter(Collision collision)
     {
         if (collision.gameObject.tag != "Knife")
             return;
+        mainCamera.BroadcastMessage("Switch");
         deadGuyAnimator.SetBool("isDead", true);
         collision.gameObject.transform.SetParent(placeholder.transform);
         collision.gameObject.BroadcastMessage("Throw1");
         collision.gameObject.GetComponent<Rigidbody>().freezeRotation = true;
         collision.gameObject.GetComponent<Rigidbody>().isKinematic = true;
         collision.gameObject.transform.rotation = Quaternion.EulerAngles(new Vector3(40, 90, 180));
+        particle.SetActive(true);
+        music1.Stop();
+        music2.Play();
+    }
+
+    public void SwitchCamera()
+    {
+        mainCamera.BroadcastMessage("Switch");
     }
 }
