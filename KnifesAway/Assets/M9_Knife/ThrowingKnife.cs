86ac7c04e83584247ba27b870520093ae732f332
Henrique Leote <200221060@estudantes.ips.pt>
2022-05-19 14:38:56 +0100

    Knife ongoing

--- KnifesAway/Assets/M9_Knife/ThrowingKnife.cs
+++ KnifesAway/Assets/M9_Knife/ThrowingKnife.cs
@@ -5,11 +5,12 @@ using UnityEngine;
 public class ThrowingKnife : MonoBehaviour
 {
 
-    public Rigidbody rigidBody;
+    private Rigidbody rigidBody;
     public float speed = 500.0F;
     public bool isActive = false;
     public GameObject placeholder;
     public GameObject mainCamera;
+    private int time = 3;
 
 
     void Start()
@@ -19,31 +20,20 @@ public class ThrowingKnife : MonoBehaviour
 
     void Update()
     {
+        
         if (isActive)
         {
             transform.SetParent(null);
-            rigidBody.velocity = new Vector3(7, -0.3F ,0);
-            transform.Rotate(0, 90.0f, 10.0f, Space.World);
-        }
+            rigidBody.velocity = new Vector3(7, -0.05F ,0);
+            
+            transform.Rotate(0, 10.0f, 0, Space.World);
+        } 
 
     }
 
     void Throw1()
     {
-        isActive = true;
+        isActive = !isActive;
     }
 
-    public void OnTriggerEnter(Collider other)
-    {
-        if(other.gameObject.tag == "Player")
-        {
-            mainCamera.BroadcastMessage("Switch");
-            isActive = false;
-            other.transform.SetParent(placeholder.transform);
-            rigidBody.velocity = new Vector3(0, 0, 0);
-            rigidBody.angularVelocity = Vector3.zero;
-            other.gameObject.SetActive(false);
-        }
-
-    }
 }
--- KnifesAway/Assets/onHit.cs
+++ KnifesAway/Assets/onHit.cs
@@ -5,20 +5,18 @@ using UnityEngine;
 public class onHit : MonoBehaviour{
 
     public GameObject parent;
-    public Animator _animator;
+    public Animator deadGuyAnimator;
+    public GameObject placeholder;
 
-    // Start is called before the first frame update
-    void Start()
-    {
-        _animator = parent.GetComponent<Animator>();
-    }
-
-    // Update is called once per frame
-    void OnCollisionEnter(Collision collision)
+    private void OnCollisionEnter(Collision collision)
     {
         if (collision.gameObject.tag != "Knife")
             return;
-
-        _animator.enabled = false;
+        deadGuyAnimator.SetBool("isDead", true);
+        collision.gameObject.transform.SetParent(placeholder.transform);
+        collision.gameObject.BroadcastMessage("Throw1");
+        collision.gameObject.GetComponent<Rigidbody>().freezeRotation = true;
+        collision.gameObject.GetComponent<Rigidbody>().isKinematic = true;
+        collision.gameObject.transform.rotation = Quaternion.EulerAngles(new Vector3(40, 90, 180));
     }
 }
